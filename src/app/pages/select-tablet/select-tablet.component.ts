8783716900dcc0ad551ba32647113ef69b7bd62e
AnnaSepita <annasepita1@gmail.com>
2018-08-27 13:11:57 +0300

    Add

--- src/app/pages/select-tablet/select-tablet.component.ts
+++ src/app/pages/select-tablet/select-tablet.component.ts
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-tablet.component.css']
 })
 export class SelectTabletComponent implements OnInit {
-
+    specification: boolean;
   constructor() {
+    this.specification = true;
   }
 
   ngOnInit() {
+  }
+
+  specifications() {
+    this.specification = false;
+    // document.getElementById('card1').style.display = 'none';
+  }
+  information() {
+      this.specification = true;
+      // document.getElementById('card1').style.display = 'block';
   }
 
 }
