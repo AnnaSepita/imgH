4bb6653958994f8e8419617f46896c2b4e6a2054
AnnaSepita <annasepita1@gmail.com>
2018-09-14 00:39:58 +0300

    Add

--- src/app/pages/select-enclosure/select-enclosure.component.ts
+++ src/app/pages/select-enclosure/select-enclosure.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-enclosure.component.css']
 })
 export class SelectEnclosureComponent implements OnInit {
-
+    slider = 9;
+    slider1 = 17;
   constructor() { }
 
   ngOnInit() {
   }
+    range(count) {
+        console.log(count.srcElement.value);
+        console.log(count.value);
+        this.slider = count.srcElement.value;
+    }
+    range1(count) {
+        console.log(count.srcElement.value);
+        console.log(count.value);
+        this.slider1 = count.srcElement.value;
+    }
 
 }
