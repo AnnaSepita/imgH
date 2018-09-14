bd06fcd78dc0af42fe45dee4b9ca3af72963d17f
AnnaSepita <annasepita1@gmail.com>
2018-09-14 17:45:52 +0300

    Add

--- src/app/pages/select-customisations/select-customisations.component.ts
+++ src/app/pages/select-customisations/select-customisations.component.ts
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class SelectCustomisationsComponent implements OnInit {
 
   constructor() { }
-
+  open = false;
+  open2 = false;
   ngOnInit() {
   }
+  select1() {
+    this.open = true;
+  }
+    select2() {
+        this.open = false;
+    }
+
+    select3() {
+        this.open2 = true;
+    }
+    select4() {
+        this.open2 = false;
+    }
 
 }
