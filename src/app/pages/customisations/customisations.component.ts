10175da1bf699699b6ecbf0a02b50d9fc9071c22
AnnaSepita <annasepita1@gmail.com>
2018-09-18 12:36:05 +0300

    Add

--- src/app/pages/customisations/customisations.component.ts
+++ src/app/pages/customisations/customisations.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customisations.component.css']
 })
 export class CustomisationsComponent implements OnInit {
-
+  open1 = false;
+    open2 = false;
+    open3 = false;
+    open4 = false;
   constructor() { }
 
   ngOnInit() {
+  }
+  menu1() {
+    this.open1 = true;
+  }
+    menu2() {
+        this.open2 = true;
+        console.log(this)
+    }
+    menu3() {
+        this.open3 = true;
+        console.log(this)
+    }
+    menu4() {
+        this.open4 = true;
+        console.log(this)
+    }
+  opn() {
+      this.open1 = false;
+      this.open2 = false;
+      this.open3 = false;
+      this.open4 = false;
   }
 
 }
--- src/app/pages/select-customisations/select-customisations.component.ts
+++ src/app/pages/select-customisations/select-customisations.component.ts
@@ -1,34 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-select-customisations',
   templateUrl: './select-customisations.component.html',
   styleUrls: ['./select-customisations.component.css'],
-    animations: [
 
-        trigger('goals', [
-            transition('* => *', [
-
-                query(':enter', style({ opacity: 0 }), {optional: true}),
-
-                query(':enter', stagger('300ms', [
-                    animate('.6s ease-in', keyframes([
-                        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-                        style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
-                        style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
-                    ]))]), {optional: true})
-                ,
-                query(':leave', stagger('300ms', [
-                    animate('.6s ease-out', keyframes([
-                        style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-                        style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
-                        style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
-                    ]))]), {optional: true})
-            ])
-        ])
-
-    ]
 })
 export class SelectCustomisationsComponent implements OnInit {
 
@@ -49,6 +25,10 @@ export class SelectCustomisationsComponent implements OnInit {
         this.open2 = true;
     }
     select4() {
+        this.open2 = false;
+    }
+    select6() {
+        this.open = false;
         this.open2 = false;
     }
 
